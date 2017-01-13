58f5287dc026da6efa06dc4253b60d9b177308e4
imhotep504 <dcamel504@gmail.com>
2017-01-13 05:18:51 -0600

    additional entry to capture puppet -> up next

--- awk.sh
+++ awk.sh
@@ -3,3 +3,6 @@
 #use awk to filter by specific field -> great for scripts 
 awk -F : '/gunna/ {print $1, $4}' /etc/passwd 
 awk -F : '/dummy/ {print $1, $7}' /etc/passwd
+
+#adding one more for puppet 
+awk -F : '/puppet/ {print $1, $4}' /etc/passwd

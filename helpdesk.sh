f9495f1b4bff5dda743d9831d54415e98a2a0320
imhotep504 <dcamel504@gmail.com>
2017-01-13 13:21:23 -0600

    Simple helpdesk script

---
+++ helpdesk.sh
@@ -0,0 +1,35 @@
+#!/bin/bash 
+
+#infinite script that runs while user in logged in 
+while true; do 
+trap "echo NOPE Int" 
+
+pinghost() 
+{
+	echo which host do you want to ping 
+	read HOSTNAME
+	ping $HOSTNAME 
+}
+
+#now prompt user for option 
+echo "Select an option" 
+select TASK in 'change passwd' 'monitor disc space' 'ping a host' 'logout' 
+do 
+	case $REPLY in 
+		1) TASK=passwd;;
+		2) TASK="df -h";;
+		3) TASK=pinghost;;
+		4) TASK=exit;; 
+	esac 
+
+#test to ensure there is a value 
+if [ -n "task" ] 
+then 	
+	$TASK
+	break 
+else 
+	echo "invalid choice #, try again please"
+fi 
+
+done 
+done  

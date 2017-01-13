e5981c9a8c00158aaa17703d88f56841850f01ce
imhotep504 <dcamel504@gmail.com>
2017-01-13 11:00:10 -0600

    Simple script for ping sweeping

---
+++ pingSweep.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+#ping sweep of approx. 15 ip addresses for server 
+#ensure only one ping otherwise will be infinite 
+echo "Now commencing ping sweep (15 addresses)" 
+for i in {10..25}; do ping -c 1 192.168.0.25; done

f16ba95e9ea46aeba25e257987f42e06197077f5
imhotep504 <dcamel504@gmail.com>
2017-01-13 11:28:17 -0600

    Simple files in directory counter script

---
+++ count.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#prompt user for directory and count how many files in directory
+echo which directory do you want to count? 
+read DIR
+cd $DIR 
+
+#index variable 
+COUNTER=0
+
+for i in * 
+do 
+	COUNTER=$((COUNTER+1))
+	echo "I have counterd $COUNTER files in this directory" 
+done 

fbd0e604ae29ae55811a89271111d411c5395948
imhotep504 <dcamel504@gmail.com>
2017-01-13 01:00:11 -0600

    added additional comments and made pretty

--- five.sh
+++ five.sh
@@ -1,14 +1,27 @@
 #!/bin/bash 
 
 #quotes and args
+#remember to supply args when running 
 
 echo "\$* gives $*" 
 echo "\$# gives $#" 
 echo "\$@ gives $@"
 echo "\$0 gives $0" 
 
+#first for loop for argument counter comparison 
+echo "starting first loop \$*"  
 for i in "$*"
 do 
 	echo $i
 done 
+
+#second for loop for $* vs $@ comparison
+echo "starting second loop \$@"
+for j in "$@"
+do 
+	echo $j
+done 
+
+#added exit code 
+exit 0 
   

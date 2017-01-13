4b3b5a77a1eed9b55bfd6800cc3081a7b7e37993
imhotep504 <dcamel504@gmail.com>
2017-01-13 11:02:06 -0600

    added status code entry

--- pingSweep.sh
+++ pingSweep.sh
@@ -4,3 +4,6 @@
 #ensure only one ping otherwise will be infinite 
 echo "Now commencing ping sweep (15 addresses)" 
 for i in {10..25}; do ping -c 1 192.168.0.25; done
+
+#adding status code entry 
+echo 0 
